67d86bbcfdb86f09ec2a7a6a2db8e8e99bbde784
Ivan <ivan.vujevic@fer.hr>
2016-05-19 22:56:05 +0200

    complement

--- scripts/make_reverse_complement.py
+++ scripts/make_reverse_complement.py
@@ -2,4 +2,5 @@ from Bio import SeqIO
 import sys
 
 for elem in SeqIO.parse(sys.argv[1],"fasta"):
+    print(">"+elem.id+"\n"+str(elem.seq) + "\n")
     print(">"+elem.id+"\n"+str(elem.seq.reverse_complement()) + "\n")
