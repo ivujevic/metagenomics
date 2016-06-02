de7a5baf533a6cc46588b0cda31eb563a9b29a82
Ivan <ivan.vujevic@fer.hr>
2016-06-02 19:23:11 +0200

    promjene

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ def test():
     tmp = sorted(tmp, reverse=True)
     k = 0
     for i in tmp:
-        print(tmp[i][0], "\t", tmp[i][1], "\t", tmp[i][2], "\t", tmp[i][3])
+        print(tmp[i])
         k += 1
         if k == 10:
             break
