16b121410c8dd9fc239d6a3635c8fc8a5463960a
Ivan <ivan.vujevic@fer.hr>
2016-06-23 22:05:26 +0200

    marker len

---
+++ scripts/refactor_marker_with_len.py
@@ -0,0 +1,18 @@
+import sys
+from Bio import SeqIO
+
+
+
+f_out = open("/home/ivujevic/Markeri/renamed_markersLen.fa","w")
+
+for elem in SeqIO.parse("/home/ivujevic/Markeri/renamed_markers1.fa","fasta"):
+    name = elem.id
+    sequence = str(elem.seq)
+    ars = name.split('|')
+    gi = ars[1]
+
+    tis = ars[3]
+    
+    f_out.write(">gi|"+gi+"|ti|"+tis + "|" + str(len(sequence))+"\n"+sequence+"\n")
+
+f_out.close()
